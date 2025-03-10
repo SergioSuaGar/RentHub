d394641faadb6b73e27eeced8297b7cfddcefdf5
Sergio Suárez <sergio.sg97@hotmail.com>
2025-03-11 00:45:54 +0100

    build: Modifico configuracion de eslint | Añado fichero configuracion para prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,52 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import pluginVue from "eslint-plugin-vue";
-import pluginPrettier from "eslint-plugin-prettier";
-import configPrettier from "eslint-config-prettier";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import pluginVue from 'eslint-plugin-vue';
+import pluginPrettier from 'eslint-plugin-prettier';
+import configPrettier from 'eslint-config-prettier';
 
 /** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
-  { files: ["**/*.{js,mjs,cjs,vue}"] },
+  {
+    files: ['**/*.{js,jsx,mjs,cjs,ts,tsx,vue}'],
+    plugins: {
+      vue: await import('eslint-plugin-vue'),
+      prettier: await import('eslint-plugin-prettier'),
+    },
+    languageOptions: {
+      globals: {
+        process: true,
+      },
+      parserOptions: {
+        ecmaVersion: 'latest',
+      },
+    },
+    rules: {
+      'prettier/prettier': [
+        'error',
+        {
+          singleQuote: true,
+          semi: true,
+          tabWidth: 2,
+          trailingComma: 'es5',
+          printWidth: 100,
+          bracketSpacing: true,
+          endOfLine: 'auto',
+        },
+      ],
+      'vue/multi-word-component-names': 'warn',
+      quotes: ['error', 'single'],
+      semi: ['error', 'always'],
+    },
+  },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
-  ...pluginVue.configs["flat/recommended"], // Cambia "essential" por "recommended" para mejores prácticas
+  ...pluginVue.configs['flat/recommended'], // Cambia "essential" por "recommended" para mejores prácticas
   {
-    name: "Prettier",
+    name: 'Prettier',
     plugins: { prettier: pluginPrettier },
     rules: {
       ...configPrettier.rules, // Desactiva reglas de ESLint que entren en conflicto con Prettier
-      "prettier/prettier": "error", // Muestra errores de Prettier en ESLint
+      'prettier/prettier': 'error', // Muestra errores de Prettier en ESLint
     },
   },
 ];
